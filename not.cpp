9d9f366c58a95dbfe92db8ed273d8898a4eea449
Ricky Arisandi <45434444+rickyarisandi046@users.noreply.github.com>
2019-01-09 13:31:45 +0700

    Add files via upload

---
+++ not.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    int w;
+    int o;
+    cout<<"x=";
+    cin>>x;
+    w=-1;
+    o =x*w;
+    if (o >=-0.5)
+    cout<<"result NOT";
+    else
+    cout<<"result NOT";
+    return 0;
+    
+}
---
+++ or.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int w1;
+    int w2;
+    int x1;
+    int x2;
+    int o;
+    cout<<"x1=";
+    cin>>x1;
+    cout<<"x2=";
+    cin>>x2;
+    w1=1;
+    w2=1;
+    o = x1*w1+x2*w2;
+    if (o >=2)
+    cout<<"result OR";
+    else
+    cout<<"result OR";
+    return 0;
+    
+}
